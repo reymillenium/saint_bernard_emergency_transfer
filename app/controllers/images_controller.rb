3e6fb8c66a164665a4287276074faf8237573c54
Reinier <reymillenium@gmail.com>
2018-12-06 04:40:15 -0500

    Added the first insertion on the db using the model image and the images_controller from the view new

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,23 @@
 class ImagesController < ApplicationController
+
+  # Muestra la vista new con el formulario para crear una nueva imagen
+  def new
+
+  end
+
+  # Crea una nueva imagen en la BD a partir de los datos recibidos desde la vista new
+  def create
+    # render plain: params[:image].inspect
+
+    @image = Image.new image_params
+    @image.save
+
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:description)
+  end
+
 end
