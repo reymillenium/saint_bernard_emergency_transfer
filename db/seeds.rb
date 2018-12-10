05ad4e13154cb5a9175d63a342371c1b1ae2ead4
Reinier <reymillenium@gmail.com>
2018-12-09 22:37:44 -0500

    Added the seeder for the allergies table

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,14 +59,67 @@ symptoms = Symptom.create([
 
 # I create a few sample observations:
 observations = Observation.create([
-                              {description: 'The patients looks a little nervous', moment: Time.now},
-                              {description: 'Coloration change in the affected area', moment: Time.now},
-                              {description: 'Do not touch the affected area', moment: Time.now},
-                              {description: 'The patient can not lift heavy weights', moment: Time.now},
-                              {description: 'The patient needs rest', moment: Time.now},
-                          ])
+                                      {description: 'The patients looks a little nervous', moment: Time.now},
+                                      {description: 'Coloration change in the affected area', moment: Time.now},
+                                      {description: 'Do not touch the affected area', moment: Time.now},
+                                      {description: 'The patient can not lift heavy weights', moment: Time.now},
+                                      {description: 'The patient needs rest', moment: Time.now},
+                                  ])
 
+# I create a few sample allergies:
+allergies = Allergy.create([
+                               # Food
+                               {description: 'coffee intolerance'},
+                               {description: 'flavored tea intolerance'},
+                               {description: 'wine intolerance'},
+                               {description: 'beer intolerance'},
+                               {description: 'gin intolerance'},
+                               {description: 'liqueurs intolerance'},
+                               {description: 'citrus intolerance'},
+                               {description: 'ketchup intolerance'},
+                               {description: 'cinnamon intolerance'},
+                               {description: 'paprika intolerance'},
+                               {description: 'curry intolerance'},
+                               {description: 'anise intolerance'},
+                               {description: 'flavored tea intolerance'},
+                               {description: 'egg intolerance'},
+                               {description: 'mango intolerance'},
+                               {description: 'strawberries intolerance'},
+                               {description: 'garlic intolerance'},
+                               {description: 'gluten intolerance'},
+                               {description: 'milk intolerance'},
+                               {description: 'lactose intolerance'},
+                               {description: 'peanut intolerance'},
+                               {description: 'rice intolerance'},
+                               {description: 'sesame intolerance'},
+                               {description: 'soy intolerance'},
+                               {description: 'Sulfites intolerance'},
+                               {description: 'wheat intolerance'},
 
+                               # Drugs
+                               {description: 'hypersensitivity to Aspirin'},
+                               {description: 'hypersensitivity to Tetracycline'},
+                               {description: 'hypersensitivity to Dilantin'},
+                               {description: 'hypersensitivity to Tegretol'},
+                               {description: 'hypersensitivity to Penicillin'},
+                               {description: 'hypersensitivity to Cephalosporins'},
+                               {description: 'hypersensitivity to Sulfonamides'},
+
+                               # Environmental
+                               {description: 'hypersensitivity to the dog hair'},
+                               {description: 'hypersensitivity to the cat hair'},
+                               {description: 'hypersensitivity to the pollen'},
+                               {description: 'hypersensitivity to the insect stings'},
+                               {description: 'hypersensitivity to the mold'},
+                               {description: 'hypersensitivity to the perfume'},
+                               {description: 'hypersensitivity to the cosmetics'},
+                               {description: 'hypersensitivity to the latex'},
+                               {description: 'hypersensitivity to the water'},
+                               {description: 'hypersensitivity to the chromium'},
+                               {description: 'hypersensitivity to the cobalt'},
+                               {description: 'hypersensitivity to the formaldehyde'},
+
+                           ])
 
 
 
